13cebf36c36a36ed8f7ef7b5559e18dbc8154011
DmytroValMan <dmitri.m2011@gmail.com>
2025-07-03 14:21:08 +0300

    initial commit for hw-3

--- src/server.js
+++ src/server.js
@@ -57,7 +57,10 @@ export const setupServer = () => {
     });
   });
 
-  app.listen(PORT, () => {
+  app.listen(PORT, (err) => {
+    if (err) {
+      throw err;
+    }
     console.log(`Server is running on port ${PORT}`);
   });
 };
